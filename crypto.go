584b299ddeb11773c0a5dac846eb23b802f46d60
naus3a <naus3a@gmail.com>
2020-01-12 19:30:08 +0100

    data size implemented in header but not used

--- crypto.go
+++ crypto.go
@@ -1,6 +1,7 @@
 package libBootleg
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"github.com/mimoo/disco/libdisco"
@@ -11,6 +12,17 @@ import (
 	"strconv"
 	"strings"
 )
+
+//packet anatony
+//Probe:
+//0
+//p
+//Text:
+//0|1..5|6..6+d
+//t|d   |D
+//File:
+//0|1|2..2+n|2+n..2+n+4|2+n+4..2+n+4+d
+//f|n|N     |d         |D
 
 //header---
 type DataType byte
@@ -26,10 +38,17 @@ type DataHeader struct {
 	dataType   DataType
 	szFileName byte
 	fileName   string
+	szData     uint32
 }
 
 func (dh *DataHeader) setText() {
 	dh.dataType = DATA_TEXT
+	dh.szData = 0
+}
+
+func (dh *DataHeader) setTextWithSize(_szText uint32) {
+	dh.setText()
+	dh.szData = _szText
 }
 
 func (dh *DataHeader) setProbe() {
@@ -40,14 +59,22 @@ func (dh *DataHeader) setFile(_fName string) {
 	dh.dataType = DATA_FILE
 	dh.szFileName = byte(len(_fName))
 	dh.fileName = _fName
+	dh.szData = 0
 }
 
+func (dh *DataHeader) setFileWithSize(_fName string, _szData uint32) {
+	dh.setFile(_fName)
+	dh.szData = _szData
+}
+
+//todo
 func (dh *DataHeader) SetFromData(_d []byte) {
 	switch _d[0] {
 	case byte(DATA_PROBE):
 		dh.dataType = DATA_PROBE
 	case byte(DATA_TEXT):
 		dh.dataType = DATA_TEXT
+
 	case byte(DATA_FILE):
 		var err error
 		dh.dataType = DATA_FILE
@@ -86,6 +113,31 @@ func (dh *DataHeader) getFileName(_d []byte) (_fn string, err error) {
 	return
 }
 
+func (dh *DataHeader) getDataSz(_d []byte) (_sz uint32, err error) {
+	dt := Byte2DataType(_d[0])
+	switch dt {
+	case DATA_TEXT:
+		if len(_d) < 6 {
+			_sz = 0
+			err = errors.New("malformed data")
+		} else {
+			_sz, err = Bytes2Uint32(_d[1:5])
+		}
+	case DATA_FILE:
+		idx := 2 + int(dh.szFileName) + 2 + 1
+		if len(_d) < idx {
+			_sz = 0
+			err = errors.New("malformed data")
+		} else {
+			_sz, err = Bytes2Uint32(_d[idx : idx+4])
+		}
+	default:
+		_sz = 0
+		err = errors.New("malformed data")
+	}
+	return
+}
+
 func (dh *DataHeader) GetType() DataType {
 	return dh.dataType
 }
@@ -98,10 +150,12 @@ func (dh *DataHeader) GetRaw() []byte {
 	switch dh.dataType {
 	case DATA_TEXT:
 		h := []byte{byte(DATA_TEXT)}
+		h = append(h, Uint322Bytes(dh.szData)...)
 		return h
 	case DATA_FILE:
 		h := []byte{byte(DATA_FILE), dh.szFileName}
 		h = append(h, []byte(dh.fileName)...)
+		h = append(h, Uint322Bytes(dh.szData)...)
 		return h
 	case DATA_PROBE:
 		h := []byte{byte(DATA_PROBE)}
@@ -123,6 +177,24 @@ func Byte2DataType(_byte byte) DataType {
 	default:
 		return DATA_NONE
 	}
+}
+
+func Bytes2Uint32(_d []byte) (val uint32, err error) {
+	if len(_d) < 4 {
+		val = 0
+		err = errors.New("makformed data")
+		return
+	} else {
+		val = binary.BigEndian.Uint32(_d)
+		err = nil
+		return
+	}
+}
+
+func Uint322Bytes(_val uint32) []byte {
+	_d := make([]byte, 4)
+	binary.BigEndian.PutUint32(_d, _val)
+	return _d
 }
 
 //---header
